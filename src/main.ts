3ed1ae4ac1608dc2238344dbee9c064c527413fe
Samantachai Naksuk <boatsamantachai@gmail.com>
2026-02-06 11:09:29 +0700

    done

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+// src/main.ts 
+import { NestFactory } from '@nestjs/core'; 
+import { AppModule } from './app.module'; 
+import { ValidationPipe } from '@nestjs/common'; 
+
+async function bootstrap() { 
+  const app = await NestFactory.create(AppModule); 
+  app.enableCors(); 
+  app.useGlobalPipes( 
+
+    new ValidationPipe({    //กรองชุดข้อมูลที่เข้ามา
+      whitelist: true, 
+      forbidNonWhitelisted: true, 
+      transform: true, 
+    }), 
+  ); 
+
+  await app.listen(process.env.PORT ?? 3000); 
+} 
+
+bootstrap(); 
